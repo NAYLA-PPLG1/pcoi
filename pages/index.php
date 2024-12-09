9504661c31ef52da2316b622096c139001a6dc8b
= <=>
2024-12-09 12:10:53 +0700

    tambahan sticky element

--- pages/index.php
+++ pages/index.php
@@ -16,6 +16,7 @@ $query = mysqli_query($koneksi, "select * from produk");
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <style>
     .lexend-<uniquifier> {
@@ -27,11 +28,20 @@ $query = mysqli_query($koneksi, "select * from produk");
 
     body {
         font-family: "Lexend", serif;
+        margin: 0;
+        padding: 0;
     }
 
     nav {
         background-color: transparent;
         backdrop-filter: blur(4px);
+    }
+
+    footer {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 
 
@@ -48,70 +58,184 @@ $query = mysqli_query($koneksi, "select * from produk");
         background-size: contain;
     }
 
+    #home {
+        height: 100vh;
+    }
+
+    #contact {
+        position: sticky;
+        bottom: 30px;
+        margin-top: 40px;
+    }
+
+    .contact__container {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+    .contact__inside {
+        padding: 12px 32px;
+        border-radius: 30px;
+    }
+
+    #contact i {
+        color: #070b15;
+        padding: 0 8px;
+    }
+
+    #informasi {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+    #informasi .info__container {
+        width: 82%;
+        text-align: center;
+    }
+
+    .info__container>.info__title {
+        padding: 0 100px;
+    }
+
+    .info__container>.info__value {
+        width: 50%;
+        margin: 60px 0;
+    }
+
+    .container__value {
+        display: flex;
+        justify-content: space-around;
+        gap: 20px;
+    }
+
+
+
+
     .child-image-content {}
 </style>
 
 <body>
-    <nav class="fixed-top navbar d-flex flex-row">
-        <div class="container d-flex font-bold text-light align-items-center">
-            <span class="navbar-brand mb-0 h1 font-bold text-dark">PCOI NAY</span>
-            <ul class="list-style-non d-flex flex-row gap-4 mb-0 text-dark align-items-center list-style-non">
-                <li><a href=""></a></li>
-                <li><a href="#home" >HOME</a></li>
-                <li><a href="#product">PRODUK</a></li>
-                <li><a href="halamanAdmin.php">DASHBOARD</a></li>
-                <?php
-                // while ($data = mysqli_fetch_array($queryUser)):
-                if (isset($_SESSION['role'])) {
-                    ?>
-                    <div class="fs-5">
-                        <?php echo $_SESSION['username'] ?>
-
-                    </div/>
-                    <a class="btn btn-danger" href="./auth/logout.php">Logout</a>
-
-                <?php } else { ?>
-                    <a class="btn btn-primary" href="./auth/login.php">login</a>
-                <?php } ?>
-            </ul>
-        </div>
-    </nav>
-    <section id="home">
-        <div class="container p-5 d-flex justify-content-center mt-3">
-            <div class="child-image-content">
-                <img src="../images/pangsit chili oil.png" alt="">
+    <header class="d-inline top-0">
+        <section id="home">
+            <div class="container p-5 d-flex justify-content-center mt-3">
+                <div class="child-image-content">
+                    <img src="../images/pcoil.png" alt="">
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <section id="product">
-        <div class="container">
-            <div class="row">
+        <nav class="sticky-top navbar d-flex flex-row bg-secondary-subtle">
+            <div class="container d-flex font-bold text-light align-items-center">
+                <span class="navbar-brand mb-0 h1 font-bold text-dark">PCOI NAY</span>
+                <ul class="list-style-non d-flex flex-row gap-4 mb-0 text-dark align-items-center list-style-non">
+                    <li><a href=""></a></li>
+                    <li><a href="#home">HOME</a></li>
+                    <li><a href="#product">PRODUK</a></li>
+                    <li><a href="halamanAdmin.php">DASHBOARD</a></li>
+                    <?php
+                    // while ($data = mysqli_fetch_array($queryUser)):
+                    if (isset($_SESSION['role'])) {
+                        ?>
+                        <div class="fs-5">
+                            <?php echo $_SESSION['username'] ?>
 
-                <div class="title my-5 d-flex justify-content-center flex-column align-items-center">
-                    <h1 class="fs-2 text-secondary">MENU</h1>
-                    <p class="">Lor ipsum domet consectetur.</p>
-                </div>
+                        </div />
+                        <a class="btn btn-danger" href="./auth/logout.php">Logout</a>
 
-                <div class="container mt-4 d-flex gap-5 row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
-                </div>
-                <?php
-                while ($data = mysqli_fetch_array($query)):
-                    ?>
-                    <div class="card p-0 row d-flex justify-content-center m-3 " style="width: 18rem;">
-                        <div class="">
-                            <img src="../assets/foto_produk/<?= $data['foto'] ?>" class="card-img-top" alt="...">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $data['nama'] ?></h5>
-                            <h5 class="card-tittle"><?= $data['harga'] ?></h5>
-                            <a href="detail.php?id=<?= $data['id'] ?>" class="btn btn-primary">Detail</a>
-                        </div>
+                    <?php } else { ?>
+                        <a class="btn btn-primary" href="./auth/login.php">login</a>
+                    <?php } ?>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section id="product">
+            <div class="container">
+                <div class="row">
+
+                    <div class="title my-5 d-flex justify-content-center flex-column align-items-center">
+                        <h1 class="fs-2 text-secondary">MENU</h1>
+                        <p class="">Lor ipsum domet consectetur.</p>
                     </div>
-                <?php endwhile ?>
+
+                    <div
+                        class="container mt-4 d-flex gap-5 row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+                    </div>
+                    <?php
+                    while ($data = mysqli_fetch_array($query)):
+                        ?>
+                        <div class="card p-0 row d-flex justify-content-center m-3 " style="width: 18rem;">
+                            <div class="">
+                                <img src="../assets/foto_produk/<?= $data['foto'] ?>" class="card-img-top" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $data['nama'] ?></h5>
+                                <h5 class="card-tittle"><?= $data['harga'] ?></h5>
+                                <a href="detail.php?id=<?= $data['id'] ?>" class="btn btn-primary">Detail</a>
+                            </div>
+                        </div>
+                    <?php endwhile ?>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+
+        <section id="informasi">
+            <div class="info__container">
+                <div class="info__title">
+                    <h1>PANGSIT CHILI OIL NAYLA</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolor minus, odit
+                        necessitatibus quod possimus sequi illo quisquam odio quasi!</p>
+                </div>
+                <div class="container__value">
+                    <div class="info__value">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores odit repellendus quasi
+                            modi rerum quidem veniam, quia quae cumque libero dolor quisquam. Tempore, sit. Tempora
+                            accusantium quae assumenda molestiae autem, voluptatem temporibus ipsam neque vitae enim
+                            corrupti necessitatibus reprehenderit quas sint asperiores corporis suscipit nihil iure
+                            mollitia officiis repudiandae omnis ducimus cum! Accusamus ipsa consectetur natus illo
+                            ratione animi iste atque, omnis aperiam rem rerum vero fugit esse facilis voluptate
+                            reprehenderit molestias, accusantium doloribus corrupti unde quos. Nam excepturi veniam est
+                            fugiat repellat blanditiis officia reprehenderit, nemo, suscipit repudiandae quaerat
+                            consectetur tempore accusantium natus, minima similique sequi libero! Blanditiis, eligendi.
+                        </p>
+                    </div>
+                    <div class="info__value">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores odit repellendus quasi
+                            modi rerum quidem veniam, quia quae cumque libero dolor quisquam. Tempore, sit. Tempora
+                            accusantium quae assumenda molestiae autem, voluptatem temporibus ipsam neque vitae enim
+                            corrupti necessitatibus reprehenderit quas sint asperiores corporis suscipit nihil iure
+                            mollitia officiis repudiandae omnis ducimus cum! Accusamus ipsa consectetur natus illo
+                            ratione animi iste atque, omnis aperiam rem rerum vero fugit esse facilis voluptate
+                            reprehenderit molestias, accusantium doloribus corrupti unde quos. Nam excepturi veniam est
+                            fugiat repellat blanditiis officia reprehenderit, nemo, suscipit repudiandae quaerat
+                            consectetur tempore accusantium natus, minima similique sequi libero! Blanditiis, eligendi.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="contact">
+            <div class="contact__container">
+                <div class="contact__inside bg-secondary-subtle">
+                    <i class="bi bi-instagram"></i>
+                    <i class="bi bi-tiktok"></i>
+                    <i class="bi bi-whatsapp"></i>
+                </div>
+            </div>
+        </section>
+
+
+    </main>
+    <footer class="bg-primer-subtle">
+                        <div>
+                            INI FOOTER BLOg
+                        </div>
+    </footer>
+
 </body>
 <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
